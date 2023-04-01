0c7462aa797629654f2dad24513d23348b529a4f
Volodymyr Yaremchak <vovkahorror@gmail.com>
2023-04-01 03:03:56 +0300

    feat: added skills icons

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -4,20 +4,90 @@ import styleContainer from '../common/styles/Container.module.scss';
 import Skill from './skill/Skill';
 import {v1} from 'uuid';
 import Title from '../common/components/title/Title';
+import {
+    SiAxios,
+    SiCss3, SiGit,
+    SiHtml5,
+    SiJavascript, SiMui, SiPostman,
+    SiReact,
+    SiReacthookform,
+    SiRedux,
+    SiSass, SiStorybook,
+    SiTypescript,
+} from 'react-icons/si';
+import {RiShieldCheckFill} from 'react-icons/ri';
 
 const Skills = () => {
     const skills = [
         {
             title: 'React',
             description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque iure repellat vero!',
+            icon: <SiReact/>,
         },
         {
-            title: 'TypeScript/JavaScript',
+            title: 'Redux',
             description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque iure repellat vero!',
+            icon: <SiRedux/>,
+        },
+        {
+            title: 'TypeScript',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque iure repellat vero!',
+            icon: <SiTypescript/>,
+        },
+        {
+            title: 'JavaScript',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque iure repellat vero!',
+            icon: <SiJavascript/>,
+        },
+        {
+            title: 'SCSS / Sass',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque iure repellat vero!',
+            icon: <SiSass/>,
         },
         {
             title: 'CSS',
             description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque iure repellat vero!',
+            icon: <SiCss3/>,
+        },
+        {
+            title: 'HTML',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque iure repellat vero!',
+            icon: <SiHtml5/>,
+        },
+        {
+            title: 'Axios',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque iure repellat vero!',
+            icon: <SiAxios/>,
+        },
+        {
+            title: 'Material UI',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque iure repellat vero!',
+            icon: <SiMui/>,
+        },
+        {
+            title: 'React Hook Form',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque iure repellat vero!',
+            icon: <SiReacthookform/>,
+        },
+        {
+            title: 'Storybook',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque iure repellat vero!',
+            icon: <SiStorybook/>,
+        },
+        {
+            title: 'Postman',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque iure repellat vero!',
+            icon: <SiPostman/>,
+        },
+        {
+            title: 'Git',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque iure repellat vero!',
+            icon: <SiGit/>,
+        },
+        {
+            title: 'Unit testing',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque iure repellat vero!',
+            icon: <RiShieldCheckFill/>,
         },
     ];
 
