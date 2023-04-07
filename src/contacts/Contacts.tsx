fd35469586ffbbbaf9e5034a942f323726c68820
Volodymyr Yaremchak <vovkahorror@gmail.com>
2023-04-08 02:04:34 +0300

    feat: changed contacts section

--- src/contacts/Contacts.tsx
+++ src/contacts/Contacts.tsx
@@ -9,12 +9,12 @@ const Contacts = () => {
         <section className={styles.contactsBlock} id={'contacts'}>
             <div className={`${styleContainer.container} ${styles.contactsContainer}`}>
                 <Title>Contacts</Title>
-                <form className={styles.form} id={'contactsForm'} action="#">
-                    <input type="text" placeholder={'Type your text...'}/>
-                    <input type="text" placeholder={'Type your text...'}/>
-                    <textarea placeholder={'Type your message...'}></textarea>
+                <form className={styles.form} id={'contactsForm'} action=".">
+                    <input type="text" placeholder={'Name'}/>
+                    <input type="email" placeholder={'E-mail'}/>
+                    <textarea placeholder={'Your message'}></textarea>
+                    <button className={styles.button} form={'contactsForm'}>Send message</button>
                 </form>
-                <button className={styles.button} form={'contactsForm'}>Submit</button>
             </div>
         </section>
     );
--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -9,7 +9,7 @@ const Main = () => {
             <div className={`${styleContainer.container} ${styles.greetingContainer}`}>
                 <div className={styles.greeting}>
                     <span>Hi there</span>
-                    <h1>I am <br/> Volodymyr Yaremchak</h1>
+                    <h1>I am <br/> <span className={styles.name}>Volodymyr Yaremchak</span></h1>
                     <p className={styles.profession}>A Frontend Developer</p>
                 </div>
                 <div className={styles.photo} style={{backgroundImage: `url(${photo})`}}></div>
