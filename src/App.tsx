bdb9b4805c6624669a704c90502d7931aabdb164
Volodymyr Yaremchak <vovkahorror@gmail.com>
2023-06-29 01:26:00 +0300

    fix: favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.scss';
 import Header from './header/Header';
 import Main from './main/Main';
@@ -10,6 +10,34 @@ import {Preloader} from './common/components/preloader/Preloader';
 
 function App() {
     const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        const setFavicon = (color: string) => {
+            const favicon = document.querySelector<HTMLLinkElement>('link[rel="icon"]');
+            if (favicon) {
+                favicon.href = `/path/to/${color}-favicon.ico`; // Замініть шлях на шлях до вашого фавікону
+            }
+        };
+
+        const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+        const handleColorSchemeChange = (e: MediaQueryListEvent | MediaQueryList) => {
+            const isDarkMode = e.matches;
+            debugger
+            if (isDarkMode) {
+                setFavicon('dark'); // Змінюємо фавікон на фавікон для темної теми
+            } else {
+                setFavicon('light'); // Змінюємо фавікон на фавікон для світлої теми
+            }
+        };
+
+        handleColorSchemeChange(darkModeMediaQuery);
+        darkModeMediaQuery.addEventListener('change', handleColorSchemeChange as (e: Event) => void);
+
+        return () => {
+            darkModeMediaQuery.removeEventListener('change', handleColorSchemeChange as (e: Event) => void);
+        };
+    }, []);
 
     return (
         <div className="App">
