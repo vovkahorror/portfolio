1232fa1652ddc72c29ea9022aa1dd64ffadda8b3
Volodymyr Yaremchak <vovkahorror@gmail.com>
2023-06-29 04:26:21 +0300

    feat: added skills

--- src/data/skills.tsx
+++ src/data/skills.tsx
@@ -7,6 +7,7 @@ import {
     SiMui,
     SiPostman,
     SiReact,
+    SiReacthookform,
     SiReactrouter,
     SiRedux,
     SiSass,
@@ -14,83 +15,86 @@ import {
     SiTypescript,
 } from 'react-icons/si';
 import {RiShieldCheckFill} from 'react-icons/ri';
-import {ReactComponent as ReactHookForm} from '../assets/icons/reactHookForm.svg';
 import React from 'react';
 
-export const skills = [
-    {
-        title: 'React',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque iure repellat vero!',
-        icon: <SiReact/>,
-    },
-    {
-        title: 'Redux / Redux Toolkit',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque iure repellat vero!',
-        icon: <SiRedux/>,
-    },
-    {
-        title: 'TypeScript',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque iure repellat vero!',
-        icon: <SiTypescript/>,
-    },
-    {
-        title: 'JavaScript',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque iure repellat vero!',
-        icon: <SiJavascript/>,
-    },
-    {
-        title: 'SCSS / Sass',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque iure repellat vero!',
-        icon: <SiSass/>,
-    },
-    {
-        title: 'CSS',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque iure repellat vero!',
-        icon: <SiCss3/>,
-    },
-    {
-        title: 'HTML',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque iure repellat vero!',
-        icon: <SiHtml5/>,
-    },
-    {
-        title: 'Axios',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque iure repellat vero!',
-        icon: <SiAxios/>,
-    },
-    {
-        title: 'React Router',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque iure repellat vero!',
-        icon: <SiReactrouter/>,
-    },
-    {
-        title: 'Material UI',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque iure repellat vero!',
-        icon: <SiMui/>,
-    },
-    {
-        title: 'React Hook Form',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque iure repellat vero!',
-        icon: <ReactHookForm/>,
-    },
-    {
-        title: 'Storybook',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque iure repellat vero!',
-        icon: <SiStorybook/>,
-    },
-    {
-        title: 'Postman',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque iure repellat vero!',
-        icon: <SiPostman/>,
-    },
-    {
-        title: 'Git',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque iure repellat vero!',
-        icon: <SiGit/>,
-    },
-    {
-        title: 'Unit testing',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque iure repellat vero!',
-        icon: <RiShieldCheckFill/>,
-    },
-];
+export const skills = {
+    mainStack: [
+        {
+            title: 'React',
+            description: 'A front-end JavaScript library for building user interfaces based on components',
+            icon: <SiReact/>,
+        },
+        {
+            title: 'Redux / Redux Toolkit',
+            description: 'A JavaScript library for managing and centralizing application state',
+            icon: <SiRedux/>,
+        },
+        {
+            title: 'TypeScript',
+            description: 'A high-level programming language that adds static typing with optional type annotations to JavaScript',
+            icon: <SiTypescript/>,
+        },
+        {
+            title: 'JavaScript',
+            description: 'A high-level, often just-in-time compiled language. It has dynamic typing, prototype-based object-orientation, and first-class functions',
+            icon: <SiJavascript/>,
+        },
+        {
+            title: 'SCSS / Sass',
+            description: 'A preprocessor scripting language that is interpreted or compiled into CSS. Provides variables, nesting, mixins, and selector inheritance',
+            icon: <SiSass/>,
+        },
+        {
+            title: 'CSS',
+            description: 'A style sheet language used for describing the presentation of a document written in a markup language',
+            icon: <SiCss3/>,
+        },
+        {
+            title: 'HTML',
+            description: 'The standard markup language for documents designed to be displayed in a web browser',
+            icon: <SiHtml5/>,
+        },
+
+    ],
+    assistiveTechnologies: [
+        {
+            title: 'Axios',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque iure repellat vero!',
+            icon: <SiAxios/>,
+        },
+        {
+            title: 'React Router',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque iure repellat vero!',
+            icon: <SiReactrouter/>,
+        },
+        {
+            title: 'Material UI',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque iure repellat vero!',
+            icon: <SiMui/>,
+        },
+        {
+            title: 'React Hook Form',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque iure repellat vero!',
+            icon: <SiReacthookform/>,
+        },
+        {
+            title: 'Storybook',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque iure repellat vero!',
+            icon: <SiStorybook/>,
+        },
+        {
+            title: 'Postman',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque iure repellat vero!',
+            icon: <SiPostman/>,
+        },
+        {
+            title: 'Git',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque iure repellat vero!',
+            icon: <SiGit/>,
+        },
+        {
+            title: 'Unit testing',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque iure repellat vero!',
+            icon: <RiShieldCheckFill/>,
+        }],
+};
--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -4,19 +4,36 @@ import Skill from './skill/Skill';
 import {v1} from 'uuid';
 import Title from '../common/components/title/Title';
 import {skills} from '../data/skills';
-import {Fade} from 'react-awesome-reveal';
+import {Fade, Zoom} from 'react-awesome-reveal';
 
 const Skills = () => {
     return (
         <section className={styles.skillsBlock} id={'skills'}>
             <div className={styles.skillsContainer}>
                 <Title>Skills</Title>
-                <Fade duration={800} cascade triggerOnce damping={0.2}>
-                    <ul className={styles.skills}>
-                        {skills.map(skill => <Skill key={v1()} title={skill.title}
-                                                    description={skill.description} icon={skill.icon}/>)}
-                    </ul>
-                </Fade>
+                <div>
+                    <Zoom triggerOnce>
+                        <h2 className={styles.title}>Core Technologies Stack</h2>
+                    </Zoom>
+                    <Fade duration={800} cascade triggerOnce damping={0.2}>
+                        <ul className={styles.skills}>
+                            {skills.mainStack.map(skill => <Skill key={v1()} title={skill.title}
+                                                                  description={skill.description} icon={skill.icon}/>)}
+                        </ul>
+                    </Fade>
+                </div>
+                <div>
+                    <Zoom triggerOnce>
+                        <h2 className={styles.title}>Assistive Technologies and Libraries</h2>
+                    </Zoom>
+                    <Fade duration={800} cascade triggerOnce damping={0.2}>
+                        <ul className={styles.skills}>
+                            {skills.assistiveTechnologies.map(skill => <Skill key={v1()} title={skill.title}
+                                                                              description={skill.description}
+                                                                              icon={skill.icon}/>)}
+                        </ul>
+                    </Fade>
+                </div>
             </div>
         </section>
     );
