ef4f5b22123428a2b9e177ec10465e60b41bc95d
Volodymyr Yaremchak <vovkahorror@gmail.com>
2023-07-04 17:49:00 +0300

    feat: added send handling

--- src/contacts/Contacts.tsx
+++ src/contacts/Contacts.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {SyntheticEvent, useState} from 'react';
 import styles from './Contacts.module.scss';
 import Title from '../common/components/title/Title';
 import {Fade} from 'react-awesome-reveal';
@@ -7,7 +7,9 @@ import {FieldError, SubmitHandler, useForm} from 'react-hook-form';
 import emailjs from '@emailjs/browser';
 import {validateEmail} from '../common/validate/validateEmail';
 import {publicKey, serviceID, templateID} from '../data/emailJSData';
-
+import {Alert, AlertColor, Snackbar} from '@mui/material';
+import {LoadingButton} from '@mui/lab';
+import SendIcon from '@mui/icons-material/Send';
 
 const Contacts = () => {
     const {ref, inView} = useInView({threshold: 1, triggerOnce: true});
@@ -16,15 +18,41 @@ const Contacts = () => {
         register,
         formState: {errors},
         handleSubmit,
+        reset,
     } = useForm<Inputs>();
 
+    const successMessage = 'Your message has been sent successfully';
+    const errorMessage = 'Your message has not been sent. Something went wrong.';
+
+    const [loading, setLoading] = useState(false);
+    const [open, setOpen] = useState(false);
+    const [severity, setSeverity] = useState<AlertColor>('success');
+    const [alertMessage, setAlertMessage] = useState(successMessage);
+
+    const handleClose = (event?: SyntheticEvent | Event, reason?: string) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpen(false);
+    };
+
     const onSubmit: SubmitHandler<Inputs> = (data) => {
+        setLoading(true);
         emailjs.send(serviceID, templateID, data, publicKey)
-            .then((result) => {
-                console.log(result.text);
+            .then(() => {
+                setSeverity('success');
+                setAlertMessage(successMessage);
+                setOpen(true);
+                reset();
             })
-            .catch((error) => {
-                console.log(error.text);
+            .catch(() => {
+                setSeverity('error');
+                setAlertMessage(errorMessage);
+                setOpen(true);
+            })
+            .finally(() => {
+                setLoading(false);
             });
     };
 
@@ -47,7 +75,7 @@ const Contacts = () => {
                     </Fade>
                     <Fade delay={600} triggerOnce>
                         <div>
-                            <input type={'email'} placeholder={'E-mail'}
+                            <input type={'text'} placeholder={'E-mail'}
                                    {...register('email', {
                                        required: 'This field is required',
                                        validate: value => validateEmail(value),
@@ -63,12 +91,26 @@ const Contacts = () => {
                         </div>
                     </Fade>
                     <Fade delay={1200} triggerOnce>
-                        <button className={styles.button} form={'contactsForm'}>
+                        <LoadingButton
+                            loading={loading}
+                            loadingPosition="end"
+                            endIcon={<SendIcon/>}
+                            variant="text"
+                            className={styles.button}
+                            type={'submit'}
+                            form={'contactsForm'}
+                        >
                             Send message
-                        </button>
+                        </LoadingButton>
                     </Fade>
                 </form>
             </div>
+
+            <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+                <Alert onClose={handleClose} variant="filled" severity={severity} sx={{width: '100%'}}>
+                    {alertMessage}
+                </Alert>
+            </Snackbar>
         </section>
     );
 };
