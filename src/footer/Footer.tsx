1b4517eefc07ae6b927ff147bb42dba9d95ad580
Volodymyr Yaremchak <vovkahorror@gmail.com>
2024-02-12 01:02:47 +0200

    fix: date of the portfolio

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -5,6 +5,8 @@ import {v1} from 'uuid';
 import {footerContacts} from '../data/footerContacts';
 
 const Footer = () => {
+    const currentYear = new Date().getFullYear();
+
     return (
         <footer className={styles.footerBlock}>
             <div className={styles.footerContainer}>
@@ -13,7 +15,7 @@ const Footer = () => {
                     {footerContacts.map(contact => <FooterContacts key={v1()} link={contact.link}
                                                                    icon={contact.icon}/>)}
                 </div>
-                <span className={styles.year}>2023</span>
+                <time className={styles.year} dateTime={String(currentYear)}>{currentYear}</time>
             </div>
         </footer>
     );
