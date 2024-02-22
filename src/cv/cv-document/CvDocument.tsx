9156f8e0c7cc5c5d8082cd2e28ffd2d95b6ed655
Volodymyr Yaremchak <vovkahorror@gmail.com>
2024-02-23 00:59:05 +0200

    feat: updated CV component

--- src/cv/cv-document/CvDocument.tsx
+++ src/cv/cv-document/CvDocument.tsx
@@ -1,9 +1,6 @@
 import resume from '../../assets/documents/Volodymyr_Yaremchak_CV.pdf';
 import {Document, Page, pdfjs} from 'react-pdf';
 import {ReactComponent as PreloaderImage} from '../../assets/icons/preloader.svg';
-import 'pdfjs-dist/build/pdf.worker.entry';
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
-import 'react-pdf/dist/esm/Page/TextLayer.css';
 import {useScreenSize} from '../../utils/useScreenSize';
 import {useMemo} from 'react';
 
@@ -16,7 +13,7 @@ export const CvDocument = (props: CvDocumentPropsType) => {
     const {mode} = props;
     const {width} = useScreenSize();
     const isMobile = width <= 576;
-    
+
     const pageWidth = useMemo(() => {
         if (isMobile) {
             return mode === 'compact' ? width / 2 : width - 20;
