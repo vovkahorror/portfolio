5c2ad653a2e3e19bc47aa1ea54fd5edb2c7c710c
Volodymyr Yaremchak <vovkahorror@gmail.com>
2024-02-24 01:13:21 +0200

    feat: updated CV component

--- src/cv/cv-document/CvDocument.tsx
+++ src/cv/cv-document/CvDocument.tsx
@@ -1,6 +1,9 @@
 import resume from '../../assets/documents/Volodymyr_Yaremchak_CV.pdf';
 import {Document, Page, pdfjs} from 'react-pdf';
 import {ReactComponent as PreloaderImage} from '../../assets/icons/preloader.svg';
+import 'pdfjs-dist/build/pdf.worker.entry';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import 'react-pdf/dist/esm/Page/TextLayer.css';
 import {useScreenSize} from '../../utils/useScreenSize';
 import {useMemo} from 'react';
 
